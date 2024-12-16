c4908ddf5855a9f2d41436bf0c397e828d68031c
Ajxd9 <134958823+Ajxd9@users.noreply.github.com>
2024-12-17 00:12:26 +0200

    Conflict resolve

--- src/main/java/fr/ujm/tse/info4/pgammon/gui/CaseButton.java
+++ src/main/java/fr/ujm/tse/info4/pgammon/gui/CaseButton.java
@@ -5,7 +5,7 @@ import javax.swing.JButton;
 
 import fr.ujm.tse.info4.pgammon.models.Square;
 
-public class SquareButton extends JButton{
+public class CaseButton extends JButton{
 	private static final long serialVersionUID = 6276324191590405443L;
 
 	public static final ImageIcon iconeNoire = new ImageIcon("images/pion_noir.png");
@@ -20,43 +20,29 @@ public class SquareButton extends JButton{
 
 	private Square c;
 	/**
-	 * 
-	 * @param _Square Square to associate with the button.
+	 *
+	 * @param _case Square to associate with the button.
 	 */
-<<<<<<< Updated upstream:src/main/java/fr/ujm/tse/info4/pgammon/gui/SquareButton.java
-	public SquareButton(Square _Square){
-		c = _Square;
-=======
 	public CaseButton(Square _case){
 		c = _case;
->>>>>>> Stashed changes:src/main/java/fr/ujm/tse/info4/pgammon/gui/CaseButton.java
 		isCandidate = false;
 	}
 
-	
-	
+
+
 	/**
-	 * 
+	 *
 	 * @return Returns the associated Square.
 	 */
-<<<<<<< Updated upstream:src/main/java/fr/ujm/tse/info4/pgammon/gui/SquareButton.java
-	public Square getSquare() {
-		return c;
-	}
-
-	public void setSquare(Square _Square) {
-		c = _Square;
-=======
 	public Square getCase() {
 		return c;
 	}
 
 	public void setCase(Square _case) {
 		c = _case;
->>>>>>> Stashed changes:src/main/java/fr/ujm/tse/info4/pgammon/gui/CaseButton.java
 	}
-	
-	
+
+
 	public boolean isCandidate() {
 		return isCandidate;
 	}
